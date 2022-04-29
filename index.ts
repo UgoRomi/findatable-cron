e554271b24d712ae164ae592e425ad70d8f89b03
Ugo Romi <ugo.romi@gmail.com>
2022-04-29 13:35:28 +0200

    Ported deno code to node

--- index.ts
+++ index.ts
@@ -1,1 +1,47 @@
-const dotenv = require('dotenv');
+import dotenv from 'dotenv';
+import { format, add } from 'date-fns';
+import nodemailer from 'nodemailer';
+import cron from 'node-cron';
+import { apiResponse } from './types';
+dotenv.config();
+
+const checkAvailability = async () => {
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.mail.me.com',
+    port: 587,
+    auth: {
+      user: 'ugo.romi@icloud.com',
+      pass: process.env.APP_PASSWORD,
+    },
+  });
+  const today = new Date();
+  const formattedFrom = format(today, 'yyyy-M-dd');
+
+  const to = add(today, { months: 7 });
+  const formattedTo = format(to, 'yyyy-M-dd');
+
+  console.log(`Checking availability from ${formattedFrom} to ${formattedTo}`);
+
+  const url = `https://api.rsvp-popup.com/api/public/restaurants/2906/availability?seats=2&from=${formattedFrom}&to=${formattedTo}`;
+  const body: apiResponse[] = await (await fetch(url)).json();
+  const availableDays = body.filter((day) => day.IsSoldOut === false);
+  console.log(`Found ${availableDays.length} available days`);
+
+  for (const availableDay of availableDays) {
+    transporter.sendMail({
+      from: 'ugo.romi@icloud.com',
+      to: 'ugo.romi@icloud.com',
+      subject: `POSTI DISPONIBILI PRESSO OSTERIA FRANCESCANA IL GIORNO ${availableDay.Date.toLocaleDateString()}`,
+      text: 'per prenotare vai su https://reservations.osteriafrancescana.it/',
+      html: '<p>per prenotare vai su <a href="https://reservations.osteriafrancescana.it/">https://reservations.osteriafrancescana.it/</a></p>',
+    });
+  }
+
+  transporter.close();
+};
+
+cron.schedule('0 * * * *', async () => {
+  console.log('Running cron job');
+  await checkAvailability();
+  console.log('Cron job completed');
+});
---
+++ types.ts
@@ -0,0 +1,9 @@
+export interface apiResponse {
+  Date: Date;
+  Grouping: string;
+  GroupingDescription: any;
+  HasInProgressBookings: boolean;
+  IsSoldOut: boolean;
+  Status: string;
+  AvailableTimes: Array<any>;
+}
